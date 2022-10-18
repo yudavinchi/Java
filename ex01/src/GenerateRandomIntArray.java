00381bf1fa26ed3fdc2671837b65d5dda577c794
yudavinchi <yudin.david@gmail.com>
2022-10-18 22:37:14 +0300

    ex01 version 1.0

---
+++ ex01/src/GenerateRandomIntArray.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class GenerateRandomIntArray {
+    private int[] arr;
+
+    public GenerateRandomIntArray(int min, int max, int length)
+    {
+        arr = new int[length];
+
+        Random rand = new Random();
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = rand.nextInt(max - min + 1) + min;
+        }
+    }
+
+    public int[] get(){
+        return arr;
+    }
+}
---
+++ ex01/src/userId.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class userId {
+    int id;
+
+    public userId(){
+        Random rand = new Random();
+
+        // random id will have 9 digits
+        id = rand.nextInt(999999999 - 900000000 ) + 100000000;
+    }
+
+    public int get(){
+        return id;
+    }
+}
